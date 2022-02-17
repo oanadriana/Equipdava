550624a20398d96cd6dd90539519ebf6cc4f093b
Oana Adriana Cireasa <Oana-Adriana.Cireasa@endava.com>
2022-02-17 14:18:19 +0200

    added GET for Employees on endpoint: api/employees

---
+++ Equipdava.Application/Employees/Queries/GetAllEmployeesQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Equipdava.Domain.Models;
+using MediatR;
+
+namespace Equipdava.Application.Employees.Queries
+{
+    public class GetAllEmployeesQuery : IRequest<IEnumerable<Employee>>
+    {
+    }
+}
---
+++ Equipdava.Application/Employees/Queries/GetAllEmployeesQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Equipdava.DB.DbContexts;
+using Equipdava.Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Equipdava.Application.Employees.Queries
+{
+    public class GetAllEmployeesQueryHandler : IRequestHandler<GetAllEmployeesQuery, IEnumerable<Employee>>
+    {
+        private readonly EquipdavaDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetAllEmployeesQueryHandler(EquipdavaDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Employee>> Handle(GetAllEmployeesQuery request, CancellationToken cancellationToken)
+        {
+            var employees = await _dbContext.Employees.ToListAsync(cancellationToken);
+
+            return _mapper.Map<IEnumerable<Employee>>(employees);
+        }
+    }
+}
